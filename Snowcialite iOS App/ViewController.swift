a306e19b507591c14cb0a017e20cdb981d5a063f
Syed Jilany <syedjilany@Syeds-MacBook-Pro.local>
2015-10-04 00:28:19 -0400

    Initial Commit with all Project Files

---
+++ Snowcialite iOS App/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Snowcialite iOS App
+//
+//  Created by Syed Jilany on 10/4/15.
+//  Copyright © 2015 Snowcialite. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
