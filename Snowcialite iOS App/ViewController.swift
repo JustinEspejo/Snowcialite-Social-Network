489fdd2364de21cf669bfbb8524052d3ca95a596
Deniz Turgut <dturgut@thelathe.com>
2015-10-11 00:27:05 -0400

    -Setup cocoa pods. -Added alamofire to pod file. -Added 2 buttons to storyboard.

--- Snowcialite iOS App/ViewController.swift
+++ Snowcialite iOS App/ViewController.swift
@@ -7,11 +7,17 @@
 //
 
 import UIKit
+import Alamofire
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var getFunctionButton: UIButton!
+    @IBOutlet weak var postFunctionButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -19,7 +25,28 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    @IBAction func getFunctionButtonTapped(sender: AnyObject)
+    {
+        Alamofire.request(.GET, "http://localhost:8081/SpringMVC/rest/kfc/brands/", parameters: ["name": "Syed"]).responseJSON
+        { response in
+                print(response.request)  // original URL request
+                print(response.response) // URL response
+                print(response.data)     // server data
+                print(response.result)   // result of response serialization
+                
+                if let JSON = response.result.value
+                {
+                    print("JSON: \(JSON)")
+                }
+        }
+        
+        print("getFunctionButtonTapped")
+    }
+    
+    @IBAction func postFunctionButtonTapped(sender: AnyObject)
+    {
+        print("postFunctionButtonTapped")
+    }
 }
 
